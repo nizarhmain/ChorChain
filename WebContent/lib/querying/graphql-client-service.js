2884df90c70f78a972423a60a09258d149af8a34
Massimiliano <massimilian.sampaolo@studenti.unicam.it>
2020-01-08 23:02:49 +0100

    Added the effective execution of the query built from the user and showed the results.

--- WebContent/lib/querying/graphql-client-service.js
+++ WebContent/lib/querying/graphql-client-service.js
@@ -2,70 +2,132 @@
 
 angular.module('querying', []).service('graphqlClientService', function ($http) {
 
-    this.availableEntities = ['Block', 'Blocks', 'Transaction', 'GasPrice'];
+    this.availableEntities = ['block', 'blocks', 'transaction', 'transactions', 'gasPrice', 'protocolVersion'];
 
     this.availableOperator = ['=', '!=', '>', '>=', '<', '<='];
-    
-    this.availableLogicalOperator = ['And', 'Or'];
 
-    this.getEntityFields = function(entity) {
-        if (entity == 'Block' || entity == 'Blocks') {
-            return [
-                'Number',
-                'Hash',
-                'Nonce',
-                'TransactionsRoot',
-                'TransactionsNonce',
-                'ReceiptsRoot',
-                'StateRoot',
-                'ExtraData',
-                'GasLimit',
-                'GasUsed',
-                'Timestamp'
-            ];
+    this.availableLogicalOperator = ['and', 'or'];
+
+    this.getEntityFields = function (entity) {
+        if (entity == 'block' || entity == 'blocks') {
+            return ['number', 'hash', 'nonce', 'transactionsRoot', 'transactionCount',
+                'receiptsRoot', 'stateRoot', 'extraData', 'gasLimit', 'gasUsed', 'timestamp',
+                'logsBloom', 'mixHash', 'difficulty', 'totalDifficulty'];
         }
-        
-        if (entity == 'Transaction' || entity == 'Transactions') {
-            return ['Hash', 'Nonce', 'Index', 'Value'];
+
+        if (entity == 'transaction' || entity == 'transactions') {
+            return ['hash', 'nonce', 'index', 'from', 'to', 'value', 'gas', 'gasPrice', 'gasUsed',
+                'cumulativeGasUsed', 'inputData', 'blockNumber', 'blockHash', 'status'];
         }
 
         return [];
     }
 
-    this.getEntityBaseRules = function(entity) {
-        if (entity == 'Block') {
+    this.getEntityBaseRules = function (entity) {
+        if (entity == 'block') {
             return [{
-                Property: 'Number',
+                Property: 'number',
                 Operator: '=',
                 Value: null,
                 Mandatory: true
             }];
         }
 
-        if (entity == 'Blocks') {
+        if (entity == 'blocks') {
             return [{
-                Property: 'Number',
+                Property: 'number',
                 Operator: '>',
                 Value: null,
-                LogicalOperator: 'And',
+                LogicalOperator: 'and',
                 Mandatory: true
             },
             {
-                Property: 'Number',
+                Property: 'number',
                 Operator: '<',
                 Value: null,
                 Mandatory: true
             }];
         }
 
-        if (entity == 'Transaction') {
+        if (entity == 'transaction') {
             return [{
-                Property: 'Hash',
+                Property: 'hash',
                 Operator: '=',
                 Value: null,
                 Mandatory: true
             }];
         }
+
+        if (entity == 'transactions') {
+            return [{
+                Property: 'blockNumber',
+                Operator: '>',
+                Value: null,
+                LogicalOperator: 'and',
+                Mandatory: true
+            },
+            {
+                Property: 'blockNumber',
+                Operator: '<',
+                Value: null,
+                Mandatory: true
+            }];
+        }
+    }
+
+    this.buildJsonQuery = function (selectedEntity, filteringRules, projectionFields) {
+        const filterExpression = buildJsonQueryFilter(filteringRules);
+        const projectionExpression = buildJsonQueryProjection(projectionFields);
+        return `{\n\t${selectedEntity}${filterExpression} ${projectionExpression}\n}`;
+    }
+
+    this.executeQuery = function (query) {
+        const request = { query: query };
+        const jsonRequest = JSON.stringify(request);
+        return $http.post('http://193.205.92.133:8547/graphql', jsonRequest);
+    }
+
+
+    function buildJsonQueryFilter(filteringRules) {
+        if (!Array.isArray(filteringRules) || filteringRules.length <= 0)
+            return '';
+
+        let filterExpression = '';
+        const mandatoryRules = filteringRules.filter(r => r.Mandatory);
+        for (const rule of mandatoryRules) {
+            const property = getFilteringRulePropertyName(rule);
+            filterExpression += `${property}: ${rule.Value},`;
+        }
+
+        filterExpression = filterExpression.slice(0, -1); // remove last unuseful comma
+        return `(${filterExpression})`;
+    }
+
+    function getFilteringRulePropertyName(rule) {
+        if (!rule || !rule.Operator)
+            return '';
+
+        if (rule.Operator == '>')
+            return 'from';
+
+        if (rule.Operator == '<')
+            return 'to';
+
+        return rule.Property;
+    }
+
+    function buildJsonQueryProjection(projectionFields) {
+        if (!projectionFields || !Object.keys(projectionFields) || Object.keys(projectionFields).length <= 0)
+            return '';
+
+        let filterExpression = '{';
+        for (const field in projectionFields) {
+            if (!projectionFields[field])
+                continue;
+            filterExpression += `\n\t\t${field}`;
+        }
+
+        return filterExpression + '\n\t}';
     }
 
 });
--- WebContent/lib/querying/querying-controller.js
+++ WebContent/lib/querying/querying-controller.js
@@ -18,38 +18,125 @@ angular.module('querying').controller('queryingController', ["$scope", "graphqlC
 
     $scope.selectedFields = {};
 
-    $scope.onEntitySelectionChanged = function() {
+    $scope.queryExecutionErrorOccurred = false;
+
+    $scope.queryExecutionErrorMessage = null;
+
+    $scope.isQuerying = false;
+
+
+    $scope.queryResult = null;
+
+    $scope.queryResults = null;
+
+    $scope.stringQueryResults = null;
+
+
+    $scope.onEntitySelectionChanged = function () {
         if (this.selectedEntity == null)
             return;
-        
+
         $scope.fields = graphqlClientService.getEntityFields(this.selectedEntity);
         $scope.rules = graphqlClientService.getEntityBaseRules(this.selectedEntity);
-        updateUI(this.selectedEntity);
+        $scope.selectedFields = {};
+        updateUI();
     }
 
-    $scope.executeQuery = function() {
-        // console.log("---------rules---------");
-        // console.log(this.rules);
-        // console.log("---------projection---------");
-        // console.log(this.selectedFields);
+    $scope.onFieldsSelectionChanged = function () { updateUI(); }
+
+    $scope.onRuleValueChanged = function () { updateUI(); }
+
+    $scope.executeQuery = function () {
+        $scope.isQuerying = true;
+        const query = graphqlClientService.buildJsonQuery(this.selectedEntity, this.rules, this.selectedFields);
+        graphqlClientService.executeQuery(query)
+            .then(queryExecuted)
+            .catch(errorExecutingQuery);
     }
 
-    function updateUI(entity) {
-        $scope.canStartQueryExecution = entity != null;
-        
-        // if (!entity) {
-        //     $scope.canStartQueryExecution = false;
-        //     return;
-        // }
-        
-        // for (const rule of $scope.rules) {
-        //     if (rule.Mandatory && rule.Value == null) {
-        //         $scope.canStartQueryExecution = false;
-        //         return;        
-        //     }
-        // }
+    $scope.closeErrorDialog = function () {
+        $scope.queryExecutionErrorOccurred = false;
+        $scope.queryExecutionErrorMessage = null;
+    }
 
-        // $scope.canStartQueryExecution = true;
+    $scope.closeResultsDialog = function () {
+        $scope.queryResults = null;
+        $scope.queryResult = null;
+        $scope.stringQueryResults = null;
+    }
+
+    function updateUI() {
+        $scope.queryExecutionErrorOccurred = false;
+        if ($scope.selectedEntity == null) {
+            $scope.canStartQueryExecution = false;
+            return;
+        }
+
+        if ($scope.selectedFields == null || Object.keys($scope.selectedFields).length <= 0 ||
+            Object.keys($scope.selectedFields).find(k => $scope.selectedFields[k] == true) == null) {
+            $scope.canStartQueryExecution = $scope.fields && $scope.fields.length <= 0;
+            return;
+        }
+
+        if (Array.isArray($scope.rules) && $scope.rules.length > 0) {
+            const mandatoryRules = $scope.rules.filter(r => r.Mandatory);
+            const brokenRule = mandatoryRules.find(r => r.Value == null || r.Value.length <= 0);
+            if (brokenRule != null) {
+                $scope.canStartQueryExecution = false;
+                return;
+            }
+        }
+
+        $scope.canStartQueryExecution = true;
+    }
+
+    function queryExecuted(response) {
+        $scope.isQuerying = false;
+        if (response.data == null || response.data['data'] == null) {
+            errorExecutingQuery(null);
+            return;
+        }
+
+        const value = response.data['data'];
+        if (Object.keys(value).length <= 0) {
+            queryEmptyResults();
+            return;
+        }
+
+        if (value.hasOwnProperty('gasPrice')) {
+            $scope.stringQueryResults = `The current gas price is ${value['gasPrice']}`;
+        } else if (value.hasOwnProperty('protocolVersion')) {
+            $scope.stringQueryResults = `The current protocolVersion is ${value['protocolVersion']}`;
+        } else {
+            showTableResults(value);
+        }
+    }
+
+    function errorExecutingQuery(_) {
+        $scope.queryExecutionErrorOccurred = true;
+        $scope.queryExecutionErrorMessage = 'An error occurred during query execution!';
+        $scope.isQuerying = false;
+    }
+
+    function queryEmptyResults() {
+        $scope.queryExecutionErrorOccurred = true;
+        $scope.queryExecutionErrorMessage = 'Query result is empty';
+        $scope.isQuerying = false;
+    }
+
+    function showTableResults(result) {
+        const keys = Object.keys(result);
+        if (!keys || keys.length <= 0) {
+            queryEmptyResults();
+            return;
+        }
+
+        const value = result[keys[0]];
+        if (Array.isArray(value)) {
+            $scope.queryResults = value;
+        } else {
+            $scope.queryResult = value;
+        }
     }
 
 }]);
